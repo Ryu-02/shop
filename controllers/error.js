b27b41eda1c5c6952bbd50b3e77b472fee1b05b1
Ryu-02 <ymq02110192@gmail.com>
2025-01-24 13:52:14 +0900

    관리자 기능 추가

---
+++ controllers/error.js
@@ -0,0 +1,5 @@
+"use strict";
+
+exports.getError = (req, res, next) => {
+  res.status(404).render("404", { pageTitle: "Page Not Found", path: "" });
+};
--- routes/admin.js
+++ routes/admin.js
@@ -2,27 +2,16 @@
 
 const path = require("path");
 
-const rootDir = require("../util/path");
+const adminController = require("../controllers/admin");
 
 const express = require("express");
 const router = express.Router();
 
-const products = [];
+// /admin/add-product => GET
+router.get("/add-product", adminController.getAddProduct);
+router.get("/products", adminController.getProduct);
 
-router.get("/add-product", (req, res, next) => {
-  res.render("add-product", {
-    pageTitle: "Add Product",
-    path: "/admin/add-product",
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true,
-  });
-});
+// /admin/add-product => POST
+router.post("/add-product", adminController.postAddProduct);
 
-router.post("/add-product", (req, res, netx) => {
-  products.push({ title: req.body.title });
-  res.redirect("/");
-});
-
-exports.routes = router;
-exports.products = products;
+module.exports = router;
